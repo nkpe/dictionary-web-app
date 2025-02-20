15e3d062987e0be4f2c588da354d1a03d4676f4d
Naomi <naomi.pearse@gmail.com>
2025-02-20 20:07:09 +0000

    search button returning api request

--- app/src/components/SearchBar.tsx
+++ app/src/components/SearchBar.tsx
@@ -1,10 +1,25 @@
 import { Search } from 'lucide-react';
+import { useState } from 'react';
 
 export const SearchBar = () => {
+    const [word, setWord] = useState("");
+
+    const sendRequest = () => {
+        // http request
+        console.log("button")
+        const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error.message))
+    }
+
     return (
         <div className="border-2 border-solid border-cyan-500 rounded-lg w-full flex p-[5px]">
-            <input type="text" placeholder="Start typing any word" id="search-input" className="w-full" />
-            <Search color="grey" />
+            <input type="text" placeholder="Start typing any word" id="search-input" className="w-full" value={word} onChange={e => setWord(e.target.value)} />
+            <button type="button" onClick={sendRequest}>
+                <Search color="grey" />
+            </button>
         </div>
     )
 }
