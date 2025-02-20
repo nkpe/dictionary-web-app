48f1a78e9c43efd0593813825364fde6810783f0
Naomi <naomi.pearse@gmail.com>
2025-02-20 20:09:50 +0000

    added enter key event to input field

--- app/src/components/SearchBar.tsx
+++ app/src/components/SearchBar.tsx
@@ -16,7 +16,11 @@ export const SearchBar = () => {
 
     return (
         <div className="border-2 border-solid border-cyan-500 rounded-lg w-full flex p-[5px]">
-            <input type="text" placeholder="Start typing any word" id="search-input" className="w-full" value={word} onChange={e => setWord(e.target.value)} />
+            <input type="text" placeholder="Start typing any word" id="search-input" className="w-full" value={word} onChange={e => setWord(e.target.value)} onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                    sendRequest();
+                }
+            }} />
             <button type="button" onClick={sendRequest}>
                 <Search color="grey" />
             </button>
